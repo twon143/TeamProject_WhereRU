b58fed02a3edf944b31ed6fd7a32fadb29a132f3
SangHyunKim <tkdgus8015@naver.com>
2018-10-24 14:40:25 +0900

    PostFragment 추가 및 PostWriteActivity UI 수정

--- app/src/main/java/edu/android/teamproject_whereru/PostWriteActivity.java
+++ app/src/main/java/edu/android/teamproject_whereru/PostWriteActivity.java
@@ -2,12 +2,34 @@ package edu.android.teamproject_whereru;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class PostWriteActivity extends AppCompatActivity {
+
+    private EditText editTitle, editBody;
+    private ImageView imageWrite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_write);
+
+        editTitle = findViewById(R.id.editTitle);
+        editBody = findViewById(R.id.editBody);
+        imageWrite = findViewById(R.id.imageWrite);
+
+
+
+    }
+
+    public void postCancle(View view) {
+        // TODO : 취소버튼
+
+    }
+
+    public void postResult(View view) {
+        // TODO : 확인버튼
     }
 }
