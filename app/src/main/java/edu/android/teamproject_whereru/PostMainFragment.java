15b32e1900b68d864b70c732a503363dbf163cf7
SangHyunKim <tkdgus8015@naver.com>
2018-10-25 16:17:56 +0900

    PostMainFragment RecyclerView 틀작업

--- app/src/main/java/edu/android/teamproject_whereru/PostMainFragment.java
+++ app/src/main/java/edu/android/teamproject_whereru/PostMainFragment.java
@@ -1,9 +1,12 @@
 package edu.android.teamproject_whereru;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +15,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.android.teamproject_whereru.Model.Post;
@@ -30,14 +36,14 @@ public class PostMainFragment extends Fragment {
         class PostViewHolder extends RecyclerView.ViewHolder {
             private ImageView imageView;
             private TextView textGuestName, textLikeCount;
-            private RecyclerView recyclerView;
+
 
             public PostViewHolder(@NonNull View itemView) {
                 super(itemView);
                 imageView = itemView.findViewById(R.id.imageView);
                 textGuestName = itemView.findViewById(R.id.textGuestName);
                 textLikeCount = itemView.findViewById(R.id.textLikeCount);
-                recyclerView = itemView.findViewById(R.id.recyclerView);
+
             }
         }
 
@@ -63,6 +69,7 @@ public class PostMainFragment extends Fragment {
                 public void onClick(View v) {
                     // 여기서는 Detail 액티비티로 넘겨줘야함
 
+
                 }
             });
 
@@ -79,14 +86,22 @@ public class PostMainFragment extends Fragment {
 
     private PostAdapter adapter;
     private List<Post> posts;
+    private RecyclerView recyclerView;
 
     private FirebaseDatabase database;
-    private DatabaseReference reference;
+    private DatabaseReference postreference;
     private ChildEventListener childEventListener;
 
 
     public PostMainFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+
     }
 
     @Override
@@ -96,17 +111,54 @@ public class PostMainFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_post_main, container, false);
 
-
-
-
         return view;
     }
 
     @Override
     public void onStart() {
         super.onStart();
+        View view = getView();
+
+        posts = new ArrayList<>();
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        database = FirebaseDatabase.getInstance();
+        postreference = database.getReference().child(""); // 저장되어있는 이름 꺼내기
 
 
+        adapter = new PostAdapter();
+        recyclerView.setAdapter(adapter);
+
+        recyclerView.setHasFixedSize(true);
+
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
 
 
 
