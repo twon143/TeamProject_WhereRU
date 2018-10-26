a7fe0f8704d599ef24cfa050a07d80c668ea83ca
OhHunSik <ohsky97@gmail.com>
2018-10-26 11:19:35 +0900

    DetailActivity 및 LoginActivity

--- app/src/main/java/edu/android/teamproject_whereru/Model/Comment.java
+++ app/src/main/java/edu/android/teamproject_whereru/Model/Comment.java
@@ -1,6 +1,8 @@
 package edu.android.teamproject_whereru.Model;
 
-public class Comment {
+import java.io.Serializable;
+
+public class Comment implements Serializable {
 
     private String commentId;
     private String postId;
--- app/src/main/java/edu/android/teamproject_whereru/PostDetailActivity.java
+++ app/src/main/java/edu/android/teamproject_whereru/PostDetailActivity.java
@@ -1,19 +1,65 @@
 package edu.android.teamproject_whereru;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.android.teamproject_whereru.Model.Comment;
 
 public class PostDetailActivity extends AppCompatActivity {
 
-    private TextView textTitle, textWritre, textDate, textTime, textRecommendation, textContent,
+    class CommentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+    // 커뮤니티 확대시 보일화면
+    private static final String DLG_TAG = "dlg";
+
+    public static final String KEY1 = "comment_key";
+    public static final String KEY2 = "image_key";
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+
+    private TextView textTitle, textWritre, textDate, textTime, textContent,
                      textComment;
 
-    private ImageView imageView;
+    private ImageView imageView, imageHeart;
     private EditText editText;
+
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +70,70 @@ public class PostDetailActivity extends AppCompatActivity {
         textWritre = findViewById(R.id.textWriter);
         textDate = findViewById(R.id.textDate);
         textTime = findViewById(R.id.textTime);
-        textRecommendation = findViewById(R.id.textRecommendation);
         textContent = findViewById(R.id.textContent);
-        textComment = findViewById(R.id.textComment);
         imageView = findViewById(R.id.imageView);
+        imageHeart = findViewById(R.id.imageHeart);
         editText = findViewById(R.id.editText);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.addItemDecoration(
+                new DividerItemDecoration(this, linearLayoutManager.getOrientation()));
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        List<Comment> comments = new ArrayList<>();
+        comments.add(new Comment("", "", "", ""));
+
+
+
     }
+
+    // PostWriteActivity에서 작성자, 날짜, 시간, 작성글을 받아와야 함
 
     public void commentRegist(View view) {
-        // 댓글 등록
+        // 댓글 등록, 삭제, 수정
+        Intent intent = new Intent(this, Comment.class);
+        String commentId = "";
+        String postId = "";
+        String guestId = "";
+        String content = editText.getText().toString();
+
+        Comment comment = new Comment(commentId, postId, guestId, content);
+        textComment.setText(comment.getContent().toString());
+
+        intent.putExtra(KEY1, comment);
+
+        startActivity(intent);
     }
 
-    public void IlikeIt(View view) {
-        // 좋아요 클릭
+    public void changeImage(View view) {
+        Intent intent = new Intent(this, PostWriteActivity.class);
+        i = 1 - i;
+        if (i == 0) {
+            imageHeart.setImageResource(R.drawable.h1);
+        } else {
+            imageHeart.setImageResource(R.drawable.h2);
+        }
+        intent.putExtra(KEY2, i);
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
