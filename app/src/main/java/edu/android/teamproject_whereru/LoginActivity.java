14800016e9dc605f9d39a372a4e14c1dea73313d
ku8230 <ku8230@naver.com>
2018-11-07 11:11:46 +0900

    UI관련 처리

--- app/src/main/java/edu/android/teamproject_whereru/LoginActivity.java
+++ app/src/main/java/edu/android/teamproject_whereru/LoginActivity.java
@@ -117,9 +117,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
                                 Toast.makeText(LoginActivity.this, "잘못된 비밀번호 입력!", Toast.LENGTH_SHORT).show();
                             }
                         }
-                        else {
-                            Toast.makeText(LoginActivity.this, "아이디 잘못됨", Toast.LENGTH_SHORT).show();
-                        }
                     } // end OnchildAdded()
 
                     @Override
